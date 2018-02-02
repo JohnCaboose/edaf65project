7a21ae5f56457114e8cd5dbbd6ed28ca3f06049d
JohnCaboose <johncaboose@live.com>
2018-02-02 14:48:58 +0100

    Changed name of server class, added main method for server program.

---
+++ src/server/network/GameServer.java
@@ -0,0 +1,18 @@
+package server.network;
+
+public class GameServer implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Starts the game server, this is the only direct contact permissible between server and client classes
+	 * @param args the parameters for starting the game server
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
