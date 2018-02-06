4c60bbbf8713da342f16097a37992846a243f9d9
Jonas <dat15jlu@student.lu.se>
2018-02-06 16:03:50 +0100

    Fix unit test library issue
    Let me know if I broke anything for someone. The tests run using the
    JUnit4 library.

--- src/test/JSONTests.java
+++ src/test/JSONTests.java
@@ -1,15 +1,15 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import com.google.gson.Gson;
 
@@ -17,54 +17,53 @@ import common.model.Coordinate;
 import common.model.GameState;
 import common.model.Snake;
 
-class JSONTests {
+public class JSONTests {
 
-	static Gson gson;
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
+	private static Gson gson;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
 		gson = new Gson();
 	}
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
 	}
 
-	@BeforeEach
-	void setUp() throws Exception {
+	@Before
+	public void setUp() throws Exception {
 	}
 
-	@AfterEach
-	void tearDown() throws Exception {
+	@After
+	public void tearDown() throws Exception {
 	}
 
 	@Test
-	void testStateConversion() {
+	public void testStateConversion() {
 		List<Snake> snakes = new ArrayList<Snake>(2);
 		Coordinate.height = 51;
 		Coordinate.width = 52;
-		Snake p1 = new Snake(new Coordinate (1,1), common.model.Direction.RIGHT,3);
-		Snake p2 = new Snake(new Coordinate (10,10), common.model.Direction.LEFT,3);		
-		
+		Snake p1 = new Snake(new Coordinate(1, 1), common.model.Direction.RIGHT, 3);
+		Snake p2 = new Snake(new Coordinate(10, 10), common.model.Direction.LEFT, 3);
+
 		snakes.add(p1);
 		snakes.add(p2);
-		
-		
+
 		GameState expectedstate = new GameState(snakes);
-		
-		
+
 		String jsonState = gson.toJson(expectedstate, GameState.class);
-		//System.out.println(jsonState);
-		
+		// System.out.println(jsonState);
+
 		GameState convertedstate = gson.fromJson(jsonState, GameState.class);
-		
-		
-		for(int i = 0; i < 2; i++) {
-			for(int k = 0; k < 3; k++) {
-				assertEquals(expectedstate.getPlayerSnakes().get(i).body.get(k), convertedstate.getPlayerSnakes().get(i).body.get(k));
+
+		for (int i = 0; i < 2; i++) {
+			for (int k = 0; k < 3; k++) {
+				assertEquals(expectedstate.getPlayerSnakes().get(i).body.get(k),
+						convertedstate.getPlayerSnakes().get(i).body.get(k));
 			}
 		}
 		assertEquals(expectedstate.getTickCounter(), convertedstate.getTickCounter());
-		
+
 	}
 
 }
--- src/test/PlayerIdentityTests.java
+++ src/test/PlayerIdentityTests.java
@@ -1,36 +1,37 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-class PlayerIdentityTests {
+public class PlayerIdentityTests {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
 	}
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
 	}
 
-	@BeforeEach
-	void setUp() throws Exception {
+	@Before
+	public void setUp() throws Exception {
 	}
 
-	@AfterEach
-	void tearDown() throws Exception {
+	@After
+	public void tearDown() throws Exception {
 	}
 
-	@org.junit.jupiter.api.Test
-	void testEnumIntConversion() {
-		assertEquals(0,common.model.PlayerIdentity.ONE.ordinal());
-		assertEquals(1,common.model.PlayerIdentity.TWO.ordinal());
-		assertEquals(2,common.model.PlayerIdentity.THREE.ordinal());
-		assertEquals(3,common.model.PlayerIdentity.FOUR.ordinal());
+	@Test
+	public void testEnumIntConversion() {
+		assertEquals(0, common.model.PlayerIdentity.ONE.ordinal());
+		assertEquals(1, common.model.PlayerIdentity.TWO.ordinal());
+		assertEquals(2, common.model.PlayerIdentity.THREE.ordinal());
+		assertEquals(3, common.model.PlayerIdentity.FOUR.ordinal());
 	}
 
 }
