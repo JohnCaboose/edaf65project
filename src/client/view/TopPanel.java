8c7c58cd7eec9c2ac1bd8008ff54cfe1f4833fa9
Jonas <dat15jlu@student.lu.se>
2018-02-09 12:06:37 +0100

    Small progress on TopPanel

--- src/client/view/TopPanel.java
+++ src/client/view/TopPanel.java
@@ -1,12 +1,23 @@
 package client.view;
 
-import javax.swing.JButton;
+import java.awt.Font;
+
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 @SuppressWarnings("serial")
 public class TopPanel extends JPanel {
+	private JTextArea colorTitleText;
+	private JTextArea aliveTitleText;
 
 	public TopPanel() {
-		add(new JButton("Top Panel"));
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		add(colorTitleText = new JTextArea("Your Color: "));
+		add(aliveTitleText = new JTextArea("Alive: "));
+		colorTitleText.setFont(new Font("arial", Font.BOLD, 24));
+		colorTitleText.setEditable(false);
+		aliveTitleText.setFont(new Font("arial", Font.BOLD, 24));
+		aliveTitleText.setEditable(false);
 	}
 }
