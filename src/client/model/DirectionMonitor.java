db8c116cc1d19bcec3ee396e1d71506645932be2
Jonas <dat15jlu@student.lu.se>
2018-02-09 17:28:16 +0100

    Sniped bug

--- src/client/model/DirectionMonitor.java
+++ src/client/model/DirectionMonitor.java
@@ -5,27 +5,27 @@ import common.model.Direction;
 import common.model.Snake;
 
 public class DirectionMonitor {
-		private Direction direction;
-		private Input input;
-	
-	public DirectionMonitor(Snake snake){
+	private Direction direction;
+	private Input input;
+
+	public DirectionMonitor(Snake snake) {
 		input = new Input(snake);
 	}
-	
+
 	public synchronized Direction getDirection() {
 		try {
-			if(direction == null) {
+			while (direction == null) {
 				wait();
 			}
-			//direction = input.keyPressed();
-		}catch(Exception e) {
+			// direction = input.keyPressed();
+		} catch (Exception e) {
 			System.out.println("Error in DirectionMonitor");
 		}
 		notifyAll();
 		return direction;
 	}
-	
+
 	public synchronized void setDirection() {
-		
+
 	}
 }
