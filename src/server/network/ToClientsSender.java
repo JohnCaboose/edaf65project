3838d35fc742e1e5be1cc383410a447482fb1de7
JohnCaboose <johncaboose@live.com>
2018-02-12 21:35:59 +0100

    ToClientsSender thread should now die when game ends.

--- src/server/network/ToClientsSender.java
+++ src/server/network/ToClientsSender.java
@@ -11,7 +11,7 @@ public class ToClientsSender implements Runnable {
 
 	@Override
 	public void run() {
-		while (true) {
+		while (!gameStateMonitor.isGameOver()) {
 			gameStateMonitor.broadcastState();
 		}
 	}
