efd78eaed2d59f8f257fcb263f2bb031b73d5cde
Daniel Myhrman <dat13dmy@alfa-11.student.lth.se>
2018-02-13 10:01:19 +0100

    not done server receiver

--- src/client/network/FromServerReciever.java
+++ src/client/network/FromServerReciever.java
@@ -1,8 +1,44 @@
 package client.network;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+import client.model.DirectionMonitor;
+import common.model.GameState;
+import common.model.PacketHandler;
+import common.model.PacketType;
+import sun.misc.IOUtils;
+
 public class FromServerReciever extends Thread {
-	@Override
+	private final DirectionMonitor directionMonitor;
+	private Socket socket;
+	private InputStream inputStream;
+	private GameState gameState;
+
+	public FromServerReciever(DirectionMonitor directionMonitor, Socket socket) {
+		this.directionMonitor = directionMonitor;
+		this.socket = socket;
+	}
+
 	public void run() {
-		// TODO Use packet handler to receive package from server.
+		try {
+			inputStream = socket.getInputStream();
+			while (true) {
+				BufferedReader buffererdreader = new BufferedReader(new InputStreamReader(inputStream));
+				String packet = "";//Will fix it
+				if(PacketHandler.stringContainsProtocolPacket(packet)){
+					if(PacketHandler.getProtocolPacketType(packet) == PacketType.GAMESTATE){
+						if(gameState != null){
+							gameState = PacketHandler.getStateFromProtocolPacket(packet);
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
