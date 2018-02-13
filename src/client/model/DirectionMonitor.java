b6cc6fd49ca77d87eaa8ad59c440f6a5b3865d9e
Daniel Myhrman <dat13dmy@alfa-11.student.lth.se>
2018-02-13 09:13:34 +0100

    minor change Dir. Monitor

--- src/client/model/DirectionMonitor.java
+++ src/client/model/DirectionMonitor.java
@@ -1,31 +1,36 @@
 package client.model;
 
+import java.io.OutputStream;
+import java.net.Socket;
+
 import client.controller.Input;
 import common.model.Direction;
+import common.model.PacketHandler;
+import common.model.PacketType;
 import common.model.Snake;
 
 public class DirectionMonitor {
 	private Direction direction;
 	private Input input;
+	private OutputStream outputStream;
+	private Socket socket;
 
 	public DirectionMonitor(Snake snake) {
 		input = new Input(snake);
+		socket = new Socket();
 	}
 
-	public synchronized Direction getDirection() {
-		try {
-			while (direction == null) {
-				wait();
-			}
-			// direction = input.keyPressed();
+	public synchronized void broadcastDirection() {
+		String message = PacketHandler.createProtocolPacket(PacketType.DIRECTION, direction.toString());
+		try{
+			
 		} catch (Exception e) {
 			System.out.println("Error in DirectionMonitor");
 		}
 		notifyAll();
-		return direction;
 	}
 
-	public synchronized void setDirection() {
-
+	public synchronized boolean directionExists() {
+		return direction != null;
 	}
 }
