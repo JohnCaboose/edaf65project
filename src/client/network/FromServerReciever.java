735d5f508b0bb0982b60c4c82e6c0efb99aae85f
saraegli <5myrorEfler>
2018-02-15 09:46:42 +0100

    Prepared to recieve GAMESTATE packets.

--- src/client/network/FromServerReciever.java
+++ src/client/network/FromServerReciever.java
@@ -14,7 +14,7 @@ public class FromServerReciever extends Thread {
 	private final DirectionMonitor directionMonitor;
 	private Socket socket;
 	private InputStream inputStream;
-	private GameState gameState;
+	private GameState gameState = null;
 
 	public FromServerReciever(DirectionMonitor directionMonitor, Socket socket) {
 		this.directionMonitor = directionMonitor;
@@ -25,20 +25,26 @@ public class FromServerReciever extends Thread {
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 			while (true) {
 				StringBuilder sb = new StringBuilder();
+				String packet ="";
+				
 				int c;
 				while ((c = reader.read()) != -1) {
 					sb.append((char) c);
 				}
 				
-				//TODO: Analyze data from server to identify packets;
-				
-				String packet = "";
-				
-				if(PacketHandler.stringContainsProtocolPacket(packet)){
-					if(PacketHandler.getProtocolPacketType(packet) == PacketType.GAMESTATE){
-						if(gameState != null){
+				if(PacketHandler.stringContainsProtocolPacket(sb.toString())) {
+					packet = PacketHandler.extractFirstPacketFromMultiPacketStringBuilder(sb);
+					PacketType type = PacketHandler.getProtocolPacketType(packet);
+					
+					switch (type) {
+						case GAMESTATE: 
 							gameState = PacketHandler.getStateFromProtocolPacket(packet);
-						}
+							break;
+						case PLAYERIDENTITY: 
+							// TODO: give the player identity to the classes which need it
+							break;
+						default:
+							System.err.println("Packet type " + type + " not supported by client.");
 					}
 				}
 			}
