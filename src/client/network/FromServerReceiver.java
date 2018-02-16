20f77030465fa6e2ce6e3efa27d1a56e647b9ef8
Jonas <dat15jlu@student.lu.se>
2018-02-16 21:44:39 +0100

    LHS of top panel can now be set during startup

--- src/client/network/FromServerReceiver.java
+++ src/client/network/FromServerReceiver.java
@@ -1,7 +1,6 @@
 package client.network;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
--- src/client/view/RunView.java
+++ src/client/view/RunView.java
@@ -5,7 +5,7 @@ import client.model.MockSnakeThread;
 public class RunView {
 	/** This run-method is used as a debugging tool for the view. */
 	public static void main(String[] args) {
-		View v = new View(null);
+		View v = new View(null, "blue");
 		Thread t = new MockSnakeThread(v);
 		t.start();
 	}
