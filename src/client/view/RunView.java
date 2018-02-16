a6a8b598ce1e7623ba8aba535ce9fa4a8f0d5259
Jonas <dat15jlu@student.lu.se>
2018-02-16 21:32:02 +0100

    Renamed Snake test thread

--- src/client/view/RunView.java
+++ src/client/view/RunView.java
@@ -1,12 +1,12 @@
 package client.view;
 
-import client.model.SnakeToViewThread;
+import client.model.MockSnakeThread;
 
 public class RunView {
 	/** This run-method is used as a debugging tool for the view. */
 	public static void main(String[] args) {
 		View v = new View(null);
-		Thread t = new SnakeToViewThread(v);
+		Thread t = new MockSnakeThread(v);
 		t.start();
 	}
 }
