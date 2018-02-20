7e2380f915d2966fcfb1ff2b42c3abc389f43163
Daniel Myhrman <dat13dmy@alfa-11.student.lth.se>
2018-02-20 10:03:44 +0100

    Identity to color conversion

--- src/client/model/PlayerIdentityColorConverter.java
+++ src/client/model/PlayerIdentityColorConverter.java
@@ -1,7 +1,33 @@
 package client.model;
 
+import client.ClientGameStateMonitor;
+import common.model.PlayerIdentity;
 
 //TODO: implement class so it returns a color based on input of PlayerIdentity
 public class PlayerIdentityColorConverter {
+	private static String color;
+	private static PlayerIdentity player;
 
+	public PlayerIdentityColorConverter(PlayerIdentity player) {
+		this.player = player;
+	}
+
+	public static String getColor() {
+		if (player == PlayerIdentity.ONE) {
+			color = "blue";
+			
+		} else if (player == PlayerIdentity.TWO) {
+			color = "green";
+		}
+		
+		else if (player == PlayerIdentity.THREE) {
+			color = "pink";
+		}
+		
+		else if (player == PlayerIdentity.FOUR) {
+			color = "orange";
+		}
+		
+		return color;
+	}
 }
