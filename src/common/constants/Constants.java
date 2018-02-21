90bb3e19549003c045d1c28a8db51d56da51eade
Jonas <dat15jlu@student.lu.se>
2018-02-21 08:00:48 +0100

    Added javadoc

--- src/common/constants/Constants.java
+++ src/common/constants/Constants.java
@@ -1,12 +1,27 @@
 package common.constants;
 
 public class Constants {
-	
+
+	/**
+	 * Defined as the amount of tiles on the play field in the horizontal
+	 * direction.
+	 */
 	public static final int BOARD_WIDTH = 24;
+
+	/**
+	 * Defined as the amount of tiles on the play field in the vertical
+	 * direction.
+	 */
 	public static final int BOARD_HEIGHT = 24;
+
+	/** Defined as the time difference between any two given server ticks. */
 	public static final int MILLIS_PER_STATE_FRAME = 400;
-	public static final int MILLIS_BETWEEN_SNAKE_GROWTHS = MILLIS_PER_STATE_FRAME*10; 
-	//MILLIS_BETWEEN_SNAKE_GROWTHS must be divisible by MILLIS_PER_STATE_FRAME
-	
+
+	/**
+	 * Defined as the frequency at which all snakes will grow. This value is
+	 * divisible by the server tick frequency, i.e.
+	 * <code>Constants.MILLIS_PER_STATE_FRAME</code>.
+	 */
+	public static final int MILLIS_BETWEEN_SNAKE_GROWTHS = MILLIS_PER_STATE_FRAME * 10;
 
 }
