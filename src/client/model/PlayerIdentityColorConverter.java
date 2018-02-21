d220e192137aab01da9d2157cdd60c9fbb8efec3
Jonas <dat15jlu@student.lu.se>
2018-02-21 08:10:30 +0100

    Refactoring

--- src/client/model/PlayerIdentityColorConverter.java
+++ src/client/model/PlayerIdentityColorConverter.java
@@ -2,31 +2,24 @@ package client.model;
 
 import common.model.PlayerIdentity;
 
-//TODO: implement class so it returns a color based on input of PlayerIdentity
 public class PlayerIdentityColorConverter {
-	private static String color;
-	private static PlayerIdentity player;
+	private static final String player1Color = "blue";
+	private static final String player2Color = "green";
+	private static final String player3Color = "pink";
+	private static final String player4Color = "orange";
 
-	public PlayerIdentityColorConverter(PlayerIdentity player) {
-		this.player = player;
-	}
-
-	public static String getColor() {
-		if (player == PlayerIdentity.ONE) {
-			color = "blue";
-			
-		} else if (player == PlayerIdentity.TWO) {
-			color = "green";
+	public static String getColor(PlayerIdentity player) {
+		switch (player) {
+		case ONE:
+			return player1Color;
+		case TWO:
+			return player2Color;
+		case THREE:
+			return player3Color;
+		case FOUR:
+			return player4Color;
+		default:
+			return "black"; /* Should not happen */
 		}
-		
-		else if (player == PlayerIdentity.THREE) {
-			color = "pink";
-		}
-		
-		else if (player == PlayerIdentity.FOUR) {
-			color = "orange";
-		}
-		
-		return color;
 	}
 }
